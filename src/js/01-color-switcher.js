36980b0ecf3818352eb289a5c05b2b06f559bdf8
LuckuGuy <alexveselovsky95@gmail.com>
2022-12-25 10:53:23 +0200

    task #1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs = {
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]'),
+}
+let timerId = null;
 
+refs.btnStart.addEventListener('click', onStartClick);
+refs.btnStop.addEventListener('click', onStopClick)
+
+
+function onStartClick() {
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+  timerId = setInterval(() => {
+    setBodyBgColor(getRandomHexColor())
+  }, 1000);
+}
+
+function onStopClick() {
+  refs.btnStop.disabled = true;
+  refs.btnStart.disabled = false;
+  clearInterval(timerId);
+}
+
+
+function setBodyBgColor(color) {
+  document.body.style.backgroundColor = color
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
